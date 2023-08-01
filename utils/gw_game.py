763194c5473d3e4f727e3caf51a40bdfebe0f644
Daniel <dcownden@gmail.com>
2023-07-31 22:27:29 -0700

    update and modularize util classes

---
+++ utils/gw_game.py
@@ -0,0 +1,439 @@
+class GridworldGame():
+  """
+  A collection methods and parameters of a gridworld game that allow
+  for interaction with and display of GridwordlBoard objects.
+  """
+  square_content = {
+      -1: "X", #Food
+      +0: "-", #Nothing
+      +1: "O"  #Critter
+      }
+
+
+  def get_square_piece(self, piece):
+    return GridworldGame.square_content[piece]
+
+
+  def __init__(self, batch_size, n_rows, n_cols,
+               num_food, lifetime, rng=None):
+    self.batch_size = batch_size
+    self.n_rows = n_rows
+    self.n_cols = n_cols
+    self.num_food = num_food
+    self.lifetime = lifetime
+    if rng is None:
+      self.rng = np.random.default_rng(seed=SEED)
+    else:
+      self.rng = rng
+
+
+  def get_init_board(self):
+    """
+    Generates a starting board given the parameters of the game.
+    Returns a tuple giving current state of the game
+    """
+    # current score, and rounds left in the episode
+    b = GridworldBoard(self.batch_size, self.n_rows, self.n_cols,
+                       self.num_food, self.lifetime, rng=self.rng)
+    return b.get_init_board_state()
+
+
+  def get_board_size(self):
+    """Shape of a sinlge board, doesn't give batch size"""
+    return (self.n_rows, self.n_cols)
+
+
+  def get_action_size(self):
+    """
+    Returns the number of all possible actions, even though only  2-4 of
+    these will ever be valid on a given turn.
+    Actions correspond to integer indexes of board locations,
+    moves to g,x,y coordinate indexes of board locations
+    """
+    return self.n_rows * self.n_cols
+
+
+  def get_batch_size(self):
+    """
+    Returns the number of actions, only 2-4 of these will ever be valid.
+    Actions correspond to integer indexes of board locations,
+    moves to x,y indexes of board locations
+    """
+    return self.batch_size
+
+
+  def string_rep(self, board, g):
+    """ A bytestring representation board g's state in the batch of boards"""
+    return (board['pieces'][g].tobytes() + board['scores'][g].tobytes() +
+            board['rounds_left'][g].tobytes())
+
+
+  def string_rep_readable(self, board, g):
+    """ A human readable representation of g-th board's state in the batch"""
+    board_s = "".join([self.square_content[square] for row in board['pieces'][g]
+                       for square in row])
+    board_s = board_s + '_' + str(board['scores'][g])
+    board_s = board_s + '_' + str(board['rounds_left'][g])
+    return board_s
+
+
+  def get_scores(self, board):
+    return board['scores'].copy()
+
+
+  def get_rounds_left(self, board):
+    return board['rounds_left'].copy()
+
+
+  def display(self, board, g):
+    """Dispalys the g-th games in the batch of boards"""
+    print("   ", end="")
+    for c_ in range(self.n_cols):
+      print(c_, end=" ")
+    print("")
+    print("-----------------------")
+    for c_ in range(self.n_cols):
+      print(c_, "|", end="")    # Print the row
+      for r_ in range(self.n_rows):
+        piece = board['pieces'][g,c_,r_]    # Get the piece to print
+        print(GridworldGame.square_content[piece], end=" ")
+      print("|")
+    print("-----------------------")
+    print("Rounds Left: " + str(board['rounds_left'][g]))
+    print("Score: " + str(board['scores'][g]))
+
+
+  def get_critter_rc(self, board, g):
+    return np.squeeze(np.array(np.where(board['pieces'][g]==1)))
+
+
+  def plot_board(self, board, g,
+                 fig=None, ax=None, critter=None, food=None, fov=None,
+                 legend_type='included',
+                 has_fov=False, #fog_of_war
+                 radius=2,
+                 figsize=(6,5),
+                 critter_name='Critter',
+                 title=None):
+    """Uses plotting functions to make picture of the current board state"""
+    batch_size, n_rows, n_cols = board['pieces'].shape
+    plt.ioff()
+    if fig is None and ax is None:
+      fig, ax = make_grid(n_rows, n_cols, figsize=figsize, title=title)
+    rc_critter = self.get_critter_rc(board, g)
+
+    if critter is None:
+      critter = plot_critter(fig, ax, rc_critter,
+                             critter_name=critter_name)
+    else:
+      critter = plot_critter(fig, ax, rc_critter,
+                             critter, critter_name=critter_name)
+    rc_food_index = np.array(np.where(board['pieces'][g] == -1))
+    rc_food_plotting = np.array(rc_food_index).T
+
+    if food is None:
+      food = plot_food(fig, ax, rc_food_plotting)
+    else:
+      food = plot_food(fig, ax, rc_food_plotting, food)
+
+    if fov is None:
+      fov = plot_fov(fig, ax, rc_critter, n_rows, n_cols,
+                     radius, has_fov)
+    else:
+      fov = plot_fov(fig, ax, rc_critter, n_rows, n_cols,
+                     radius, has_fov, fov)
+
+    if legend_type == 'included':
+      fig.legend(loc = "outside right upper")
+      fig.canvas.draw()
+      return fig, ax, critter, food, fov
+    elif legend_type == 'separate':
+      fig_legend, ax_legend = plt.subplots(figsize=(1.5,1.5), layout='constrained')
+      fig_legend.get_layout_engine().set(w_pad=0, h_pad=0, hspace=0, wspace=0)
+      handles, labels = ax.get_legend_handles_labels()
+      ax_legend.legend(handles, labels, loc='center')
+      ax_legend.axis('off')
+      fig_legend.canvas.header_visible = False
+      fig_legend.canvas.toolbar_visible = False
+      fig_legend.canvas.resizable = False
+      fig_legend.canvas.footer_visible = False
+      fig_legend.canvas.draw()
+      return fig, ax, critter, food, fov, fig_legend, ax_legend
+    else: #no legend
+      fig.canvas.draw()
+      return fig, ax, critter, food, fov
+
+
+  def get_valid_actions(self, board):
+    """
+    A Helper function to translate the g,x,y, tuples provided the
+    GridworldBoard.get_legal_moves method into valid actions, represented
+    as binary vectors of len num_actions.
+
+    Args:
+      board: a triple of np arrays representing board state
+        pieces,       - batch_size x n_rows x n_cols
+        scores,       - batch_size
+        rounds_left   - batch_size
+
+    Returns:
+      valids: np.ndarray(binary) batch_size x num_actions, 1's represent
+              valid moves
+    """
+    batch_size, n_rows, n_cols = board['pieces'].shape
+    b = GridworldBoard(batch_size, n_rows, n_cols,
+                       self.num_food, self.lifetime,
+                       rng=self.rng)
+    b.set_state(board)
+    legal_moves =  b.get_legal_moves()
+    valids = np.zeros((batch_size, n_rows * n_cols))
+    for ii, g in enumerate(legal_moves[:,1:,:]):
+      for x,y in zip(g[0],g[1]):
+        valids[ii, x*self.n_cols+y] = 1
+    return valids
+
+
+  def display_moves(self, board, g):
+    """Dispaly possible moves for the g-th games in the batch of boards"""
+    batch_size, n_rows, n_cols = board['pieces'].shape
+    A=np.reshape(self.get_valid_actions(board)[g], (n_rows, n_cols))
+    print("  ")
+    print("possible moves")
+    print("   ", end="")
+    for col in range(self.n_cols):
+      print(col, end=" ")
+    print("")
+    print("-----------------------")
+    for col in range(self.n_cols):
+      print(col, "|", end="")    # Print the row
+      for row in range(self.n_rows):
+        piece = A[col][row]    # Get the piece to print
+        print(GridworldGame.square_content[piece], end=" ")
+      print("|")
+    print("-----------------------")
+
+
+  def get_perceptions(self, board, radius):
+    batch_size, n_rows, n_cols = board['pieces'].shape
+    b = GridworldBoard(batch_size, n_rows, n_cols,
+                       self.num_food, self.lifetime,
+                       rng=self.rng)
+    b.set_state(board)
+    return(b.get_perceptions(radius))
+
+
+  def get_next_state(self, board, actions, a_indx=None):
+    """
+    Helper function using GridworldBoard.execute_moves to update board state
+    given actions on a batch of boards
+
+    Args:
+      board: a triple of np arrays representing board state
+        pieces,       - batch_size x n_rows x n_cols
+        scores,       - batch_size
+        rounds_left   - batch_size
+      actions: list of integer indexes of critter's new board positions
+      a_indx: list of integer indexes indicating which actions are being taken
+        on which boards in the batch
+
+    Returns:
+      a board tiple signifying next state
+
+    Note:
+      if len(actions) > batch_size of board the returned board state will have
+      an expanded batch size, allowing multiple paths in the game tree to be
+      explored in parellel
+
+    """
+    batch_size, n_rows, n_cols = board['pieces'].shape
+    if board['rounds_left'][0] <= 0:
+      # assumes all boards in the batch have the same rounds left
+      # no rounds left return the board unchanged
+      return board
+    else:
+      moves = self.actions_to_moves(actions)
+      b = GridworldBoard(len(actions), n_rows, n_cols,
+                         self.num_food, self.lifetime,
+                         rng=self.rng)
+      if a_indx is None:
+        # just one move on each board in the batch
+        assert batch_size == len(actions)
+        b.set_state(board)
+      else:
+        # potentially multiple moves on each board, expand the batch
+        assert len(actions) == len(a_indx)
+        newPieces = np.array([board['pieces'][ai].copy() for ai in a_indx])
+        newScores = np.array([board['scores'][ai].copy() for ai in a_indx])
+        newrounds_left = np.array([board['rounds_left'][ai].copy() for ai in a_indx])
+        b.set_state((newPieces, newScores, newrounds_left))
+      b.execute_moves(moves)
+      return b.get_state()
+
+
+  def actions_to_moves(self, actions):
+    """
+    Args:
+      actions: a batch length list of integer indexes for the flattened boards,
+      i.e. in the range(n_cols * n_rows) actions are often much easier to use
+      as training targets for NN based RL agents.
+    Returns
+      moves: a 3-tuple of 1-d arrays each of length batch_size,
+        the first array gives the specific board within the batch,
+        the second array in the tuple gives the new row coord for each critter
+        on each board and the third gives the new col coord. Note this is
+        exactly the format expected by GridworldBoard.execute_moves, and
+        is a canonical way of indexing arrays for quick numpy operations.
+    """
+    moves = (np.arange(len(actions)),
+             np.floor_divide(actions, self.n_cols),
+             np.remainder(actions, self.n_cols))
+    return moves
+
+
+  def moves_to_actions(self, moves):
+    """
+    Args:
+      moves: a 3-tuple of 1-d arrays each of length batch_size,
+        the first array gives the specific board within the batch,
+        the second array in the tuple gives the new row coord for each critter
+        on each board and the third gives the new col coord. Note this is
+        exactly the format expected by GridworldBoard.execute_moves, and
+        is a canonical way of indexing arrays for quick numpy operations.
+    Returns:
+      actions: a batch length list of integer indexes for the flattened boards,
+      i.e. in the range(n_cols * n_rows) actions are often much easier to use
+      as training targets for NN based RL agents.
+    """
+    _, rows, cols = moves
+    actions = rows * self.n_cols + cols
+    return actions
+
+
+  def critter_oriented_get_next_state(self, board, offsets):
+    """
+    Translates directions in reference to the critter's location into
+    moves on the board in absolute terms, while checking for
+    bouncing/reflecting then get's the next state.
+
+    Args:
+      board: a triple of np arrays representing board state
+        pieces,       - batch_size x n_rows x n_cols
+        scores,       - batch_size
+        rounds_left   - batch_size
+      offsets: batch length list of strings one 'up', 'down', 'left', 'right'
+
+    Returns:
+      a board triple signifying next state
+
+    Note:
+      Unlike get_next_state, this method does not allow for expansion
+      of the game tree, i.e. len(offsets)==batch_size required
+    """
+    assert len(offsets) == board['pieces'].shape[0]
+    batch_size, n_rows, n_cols = board['pieces'].shape
+    b = GridworldBoard(batch_size, n_rows, n_cols,
+                       self.num_food, self.lifetime,
+                       rng=self.rng)
+    b.set_state(board)
+    moves = self.critter_direction_to_move(board, offsets)
+    b.execute_moves(moves)
+    return(b.get_state())
+
+
+  def critter_direction_to_move(self, board, offsets):
+    """
+    Translates directions in reference to the critter's location into
+    moves on the board in absolute terms, while checking for
+    bouncing/reflecting then returns moves.
+
+    Args:
+      board: dict of np arrays representing board state
+        'pieces':       batch_size x n_rows x n_cols
+        'scores':       batch_size
+        'rounds_left':  batch_size
+      offsets: batch length list of strings,
+        one of 'up', 'down', 'left', 'right'
+
+    Returns:
+      moves: a 3-tuple of 1-d arrays each of length batch_size,
+        the first array gives the specific board within the batch,
+        the second array in the tuple gives the new row coord for each critter
+        on each board and the third gives the new col coord. Note this is
+        exactly the format expected by GridworldBoard.execute_moves, and
+        is a canonical way of indexing arrays for numpy.
+
+    Note:
+      Unlike get_next_state, this method does not allow for expansion
+      of the game tree, i.e. len(offsets)==batch_size required
+    """
+    assert len(offsets) == board['pieces'].shape[0]
+    batch_size, n_rows, n_cols = board['pieces'].shape
+    offset_dict = {'up': (0, -1, 0),
+                   'down': (0, 1, 0),
+                   'left': (0, 0, -1),
+                   'right': (0, 0, 1)}
+    critter_locs = np.where(board['pieces'] == 1)
+    offsets_array = np.hstack([np.array(offset_dict[ost_]).reshape((3,1))
+                           for ost_ in offsets])
+    new_locs = np.array(critter_locs) + offsets_array
+    #check bounces at boundaries
+    new_locs[1,:] = np.where(new_locs[1] >=
+                               n_rows, n_rows-2, new_locs[1])
+    new_locs[2,:] = np.where(new_locs[2,:] >=
+                               n_cols, n_cols-2, new_locs[2,:])
+    new_locs[1,:] = np.where(new_locs[1,:] < 0, 1, new_locs[1,:])
+    new_locs[2,:] = np.where(new_locs[2,:] < 0, 1, new_locs[2,:])
+    moves = tuple(new_locs)
+    return moves
+
+
+  def action_to_critter_direction(self, board, actions):
+    """
+    Translates an integer index action into up/down/left/right
+
+    Args:
+      board: a triple of np arrays representing board state
+        pieces,       - batch_size x n_rows x n_cols
+        scores,       - batch_size
+        rounds_left   - batch_size
+      actions: a batch size ndarry of integer indexes for actions on each board
+
+    Returns:
+      offsets: a batch length list of strings 'up', 'down', 'left', 'right'
+    """
+    offset_dict = {(0, 0, 1): 'right',
+                   (0, 0,-1): 'left',
+                   (0, 1, 0): 'down',
+                   (0,-1, 0): 'up'}
+    batch_size, n_rows, n_cols = board['pieces'].shape
+    critter_locs = np.where(board['pieces'] == 1)
+    moves = (np.arange(len(actions)),
+               np.floor_divide(actions, n_cols),
+               np.remainder(actions, n_cols))
+    # need to reverse this from above, moves is equiv to new_locs
+    # new_locs = np.array(critter_locs) + offsets_array
+    offsets_array = np.array(moves) - np.array(critter_locs)
+    offsets = [offset_dict[tuple(o_)] for o_ in offsets_array.T]
+    return offsets
+
+
+  def get_game_ended(self, board):
+    """
+    Helper function to signify if game has ended
+
+    Args:
+      board: a triple of np arrays representing board state
+        pieces,       - batch_size x n_rows x n_cols
+        scores,       - batch_size
+        rounds_left   - batch_size
+
+    Returns:
+      batch size np.array of -1 if not ended, and scores for
+      each game in the batch if it is ended
+    """
+    rounds_left = board['rounds_left']
+    scores = board['scores']
+    if np.any(rounds_left >= 1):
+      return np.ones(self.batch_size) * -1.0
+    else:
+      return scores
---
+++ utils/gw_plotting.py
@@ -0,0 +1,133 @@
+def make_grid(num_rows, num_cols, figsize=(7,6), title=None):
+  """Plots an n_rows by n_cols grid with cells centered on integer indices and
+  returns fig and ax handles for futher use
+  Args:
+    num_rows (int): number of rows in the grid (vertical dimension)
+    num_cols (int): number of cols in the grid (horizontal dimension)
+
+  Returns:
+    fig (matplotlib.figure.Figure): figure handle for the grid
+    ax: (matplotlib.axes._axes.Axes): axes handle for the grid
+  """
+  fig, ax = plt.subplots(figsize=figsize, layout='constrained')
+  fig.get_layout_engine().set(w_pad=4 / 72, h_pad=4 / 72, hspace=0, wspace=0)
+  ax.spines[['right', 'top']].set_visible(True)
+  ax.set_xticks(np.arange(0, num_cols, 1))
+  ax.set_yticks(np.arange(0, num_rows, 1))
+  # Labels for major ticks
+  ax.set_xticklabels(np.arange(0, num_cols, 1),fontsize=8)
+  ax.set_yticklabels(np.arange(0, num_rows, 1),fontsize=8)
+
+  # Minor ticks
+  ax.set_xticks(np.arange(0.5, num_cols-0.5, 1), minor=True)
+  ax.set_yticks(np.arange(0.5, num_rows-0.5, 1), minor=True)
+
+  ax.xaxis.tick_top()
+
+  # Gridlines based on minor ticks
+  ax.grid(which='minor', color='grey', linestyle='-', linewidth=2, alpha=0.5)
+
+  # Remove minor ticks
+  ax.tick_params(which='minor', bottom=False, left=False)
+
+  ax.set_xlim(( -0.5, num_cols-0.5))
+  ax.set_ylim(( -0.5, num_rows-0.5))
+  ax.invert_yaxis()
+  if title is not None:
+    fig.suptitle(title)
+  fig.canvas.header_visible = False
+  fig.canvas.toolbar_visible = False
+  fig.canvas.resizable = False
+  fig.canvas.footer_visible = False
+  fig.canvas.draw()
+  return fig, ax
+
+
+def plot_food(fig, ax, rc_food_loc, food=None):
+  """
+  Plots "food" on a grid implied by the given fig, ax arguments
+
+  Args:
+    fig, ax: matplotlib figure and axes objects
+    rc_food_loc: ndarry(int) of shape (N:num_food x 2:row,col)
+    food: a handle for the existing food matplotlib PatchCollenction object
+    if one exists
+  Returns:
+    a handle for matplotlib PathCollection object of food scatter plot, either
+    new if no handle was passed or updated if it was
+  """
+  # if no PathCollection handle passed in:
+  if food is None:
+    food = ax.scatter([], [], s=150, marker='o', color='red', label='Food')
+  rc_food_loc = np.array(rc_food_loc, dtype=int)
+  #matrix indexing convention is is [row-vertical, col-horizontal]
+  #plotting indexing convention is (x-horizontal,y-vertical), hence flip
+  food.set_offsets(np.fliplr(rc_food_loc))
+  return food
+
+
+def plot_critter(fig, ax, rc_critter_loc,
+                 critter=None, critter_name='Critter'):
+  """
+  Plots "critter" on a grid implied by the given fig, ax arguments
+
+  Args:
+    fig, ax: matplotlib figure and axes objects
+    rc_critter_loc: ndarry(int) of shape (N:num_critters x 2:row,col)
+    critter: a handle for the existing food matplotlib PatchCollenction object
+    if one exists
+  Returns:
+    a handle for matplotlib PathCollection object of critter scatter plot,
+    either new if no handle was passed in or updated if it was.
+  """
+  if critter is None:
+    critter = ax.scatter([], [], s=250, marker='h',
+                         color='blue', label=critter_name)
+  # matrix indexing convention is is [row-vertical, col-horizontal]
+  # plotting indexing convention is (x-horizontal,y-vertical), hence flip
+  critter.set_offsets(np.flip(rc_critter_loc))
+  return critter
+
+
+def plot_fov(fig, ax, rc_critter, n_rows, n_cols, radius, has_fov, fov=None):
+  """
+  Plots a mask on a grid implied by the given fig, ax arguments
+
+  Args:
+    fig, ax: matplotlib figure and axes objects
+    rc_critter: ndarry(int) (row,col) of the critter
+    mask: a handle for the existing mask matplotlib Image object if one exists
+  Returns:
+    a handle for matplotlib Image object of mask, either new if no handle
+    was passed in or updated if it was.
+  """
+
+  # Initialize mask as a semi-transparent overlay for the entire grid
+  mask_array = np.ones((n_rows, n_cols, 4))
+  mask_array[:, :, :3] = 0.5  # light grey color
+  if has_fov == True:
+    mask_array[:, :, 3] = 0.5  # 50% opacity
+    # Create arrays representing the row and column indices
+    rows = np.arange(n_rows)[:, np.newaxis]
+    cols = np.arange(n_cols)[np.newaxis, :]
+    # Iterate over each critter location
+    dist = np.abs(rows - rc_critter[0]) + np.abs(cols - rc_critter[1])
+    # Set the region within the specified radius around the critter to transparent
+    mask_array[dist <= radius, 3] = 0
+  else:
+    mask_array[:, :, 3] = 0
+
+  if fov is None:
+    fov = ax.imshow(mask_array, origin='lower', zorder=2)
+  else:
+    fov.set_data(mask_array)
+
+  return fov
+
+
+def remove_ip_clutter(fig):
+  fig.canvas.header_visible = False
+  fig.canvas.toolbar_visible = False
+  fig.canvas.resizable = False
+  fig.canvas.footer_visible = False
+  fig.canvas.draw()
